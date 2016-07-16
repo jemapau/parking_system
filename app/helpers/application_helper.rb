0f6307a70c85029eb260a321c790c0df13545e5b
jemapau <je.mapau@gmail.com>
2016-07-15 20:58:34 -0500

    Adding layouts of the site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def alert_class_for(flas_type)
+		{
+			:success => 'alert-success',
+			:error => 'alert-danger',
+			:alert => 'alert-warning',
+			:notice => 'alert-info'
+		}
+
+		[flas_type.to_sym] || flas_type.to_s
+	end
 end
