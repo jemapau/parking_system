b174b5222cbb75b5cb8c39bf5de7867c2cd40986
jemapau <je.mapau@gmail.com>
2016-07-22 20:53:02 -0500

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,17 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :vehicles
 
+<<<<<<< HEAD
 	validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }
 	validates :last_name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
     message: "only allows letters" }
 	validates_date :birthdate, :on_or_before => lambda { Date.current }
+=======
+	validates :email, presence: true, uniqueness: true
+	validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+	validates :first_name, presence: true, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+>>>>>>> validation
 end
