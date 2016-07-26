b60829e76b860f000b64570d6f72c608a633b79e
jemapau <je.mapau@gmail.com>
2016-07-25 20:14:22 -0500

    Adding gem location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,9 @@
 class Location < ActiveRecord::Base
+	validates :address, presence: true, length: { maxinum: 255 }
+
 	has_many :slots
+
+	after_validation :geocode, if: ->(obj) { obj.address.present? && obj.address_change?}
+	geocoded by :address
+	reverse_geocoded by :latitude, :longitude
 end
