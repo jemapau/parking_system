2b22110d545ff091c1d2ed123620bbde57ea2ef5
jemapau <je.mapau@gmail.com>
2016-07-25 20:25:00 -0500

    Location file updated.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,9 @@
 class Location < ActiveRecord::Base
-	validates :address, presence: true, length: { maxinum: 255 }
+	validates :address, presence: true
 
 	has_many :slots
 
-	after_validation :geocode, if: ->(obj) { obj.address.present? && obj.address_change?}
-	geocoded by :address
-	reverse_geocoded by :latitude, :longitude
+	after_validation :geocode, if: ->(obj) { obj.address.present? && obj.address_changed?}
+	geocoded_by :address
+	reverse_geocoded_by :latitude, :longitude
 end
