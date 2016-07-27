e4e48557940de6d3eac90df82443e0758561d761
jemapau <je.mapau@gmail.com>
2016-07-26 19:19:07 -0500

    Locations views and registration changes.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,4 +3,25 @@ class LocationsController < ApplicationController
 		@locations = Location.all
 		flash[:error] = 'No locations registered' if.@Locations.empty?
 	end
+	def new
+		@user = current_user
+		@location = Location.new
+	end
+	def create
+		@location = Location.new(location_params)
+		@location.user_id = current_user.id
+		if @location.save
+			flash[:success] = 'Vehicle registered successfully.'
+			redirect_to user_locations_path(current_user)
+		else
+			flash[:error] = @location.errors.full_messages.join('.')
+			render 'new'
+		end
+	end
+
+		def location_params
+			params.require(:name).permit(:latitude, :longitude, :description, :max_slots, :address)
+		end
+		private :location_params
+	end
 end
