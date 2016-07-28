175bdfa5eb07553370c025b65fa56e04b497a7ae
jemapau <je.mapau@gmail.com>
2016-07-28 18:38:48 -0500

    Location views.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,27 +1,24 @@
 class LocationsController < ApplicationController
 	def index
 		@locations = Location.all
-		flash[:error] = 'No locations registered' if.@locations.empty?
+		flash[:error] = 'No locations registered' if @locations.empty?
 	end
 	def new
-		@user = current_user
 		@location = Location.new
 	end
 	def create
 		@location = Location.new(location_params)
-		@location.user_id = current_user.id
 		if @location.save
 			flash[:success] = 'Location registered successfully.'
-			redirect_to user_locations_path(current_user)
+			redirect_to locations_path
 		else
 			flash[:error] = @location.errors.full_messages.join('.')
 			render 'new'
 		end
 	end
 
-		def location_params
-			params.require(:name).permit(:latitude, :longitude, :description, :max_slots, :address)
-		end
-		private :location_params
+	def location_params
+			params.require(:location).permit(:name, :latitude, :longitude, :description, :max_slots, :address)
 	end
+		private :location_params
 end
