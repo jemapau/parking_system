8a7137c411f5acaf71b88a3a6010dabfea3e2da1
jemapau <je.mapau@gmail.com>
2016-08-05 19:00:15 -0500

    Spec location and user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,44 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	describe 'relations' do
-		context 'when user has vehicles'
-			it 'returns related vehicles' do
-				let(:first_name){'Mock'}
-				let(:last_name){'User'}
-				let(:password){'123456'}
-				let(:email){'mock_user@miemail.com'}
-				let(:birthdate){Date.new('1988,12,06')}
+  describe 'relations' do
+    let(:first_name) { 'Mock' }
+    let(:last_name) { 'User' }
+    let(:password) { '123456' }
+    let(:email) { 'mock_user@miemail.com' }
+    let(:birthdate) { Date.new(1987, 12, 06) }
 
-				let (:user) {
-					User.new (first_name: first_name,
-							last_name: last_name,
-							password: password,
-							email: email,
-							birthdate: birthdate)
-				}
-			end
+    let(:user) {
+                  User.new(first_name: first_name,
+                           last_name: last_name,
+                           password: password,
+                           email: email,
+                           birthdate: birthdate)
+                            }
+    before do
+      user.save
+    end
 
-			before do
-				user.save
-			end
+    context 'when user has vehicles' do
+      it 'returns related vehicles' do
+        Vehicle.create(vin: 'MOCK879', year: 1998, model: 'Chrysler', user: user)
+        expect(user.vehicles).not_to be_empty
+      end
+      it 'returns number of related vehicles' do
+        Vehicle.create(vin: 'MOCK879', year: 1998, model: 'Chrysler', user: user)
+        Vehicle.create(vin: 'AURM123', year: 2008, model: 'Chrysler', user: user)
+        expect(user.vehicles.length).to eq 2
+      end
+    end
+    context 'when user has no vehicles' do
+      it 'returns no vehicles' do
+        expect(user.vehicles).to be_empty
+      end
+    end
+  end
+  describe 'validations' do
+    let(:first_name) { 'Mock' }
+    let(:last_name) { 'User' }
+    let(:password) { '123456' }
+    let(:email) { 'mock_user@miemail.com' }
+    let(:birthdate) { Date.new(1987, 12, 06) }
 
-			context 'when user has vehicle' do
-				it 'returns related vehicles' do 
-					Vehicle.create(vin: 'MOCK879', year: 1988, model: 'Chrysler', user: user)
-					expect(user.vehicles).not_to be_empty
-				end
-				it 'returns number of related vehicles' do
-					Vehicle.create(vin: 'MOCK879', year: 1988, model: 'Chrysler', user: user)
-					Vehicle.create(vin: 'AURM123', year: 2008, model: 'Chrysler', user: user)
-					expect(user.vehicles.length).to eq 2
-				end
-			end
-		end
-		context
-			it 'returns no vehicle' do
-				expect(user.vehicles).to be_empty
-			end
-		end 
-	end	
+    let(:user) {
+                  User.new(first_name: first_name,
+                           last_name: last_name,
+                           password: password,
+                           email: email,
+                           birthdate: birthdate)
+                            }
+
+    context 'when user is created with a birthdate greater than today' do
+      it 'does not allow saving the user' do
+        expect(user.vehicles).not_to be_empty
+      end
+    end
+    context 'when user has no vehicles' do
+      it 'returns no vehicles' do
+        expect(user.vehicles).to be_empty
+      end
+    end
+  end
 end
